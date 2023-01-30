b715c11466da3150dca6e76db552ef54cd2408a8
daniel5629 <apxls34@gmail.com>
2023-01-31 00:07:08 +0900

    feat: tag test

--- src/main/kotlin/com/dn/inventory/controller/HealthCheckController.kt
+++ src/main/kotlin/com/dn/inventory/controller/HealthCheckController.kt
@@ -1,9 +1,11 @@
 package com.dn.inventory.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Tag(name = "3. HealthCheck API")
 class HealthCheckController {
 
     @GetMapping("/")
